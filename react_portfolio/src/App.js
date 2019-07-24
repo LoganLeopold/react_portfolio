c2f9161a078cce1d3b6cae898d1f4604451cf858
Logan Leopold <loganleopld@me.com>
2019-07-24 11:55:46 -0400

    adding pages I think I need

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -8,13 +8,15 @@ function App() {
   <div>
     <div>This is a placeholder lollerskaat</div>
     <nav className='top'>
-     <h1> <Link className='homepage' to='/'>Home</Link></h1>
-     <h2> <Link to={'/project/'}>Project 1</Link></h2> 
-     </nav>  
+      <h1> <Link className='homepage' to={'/'}>          Home</Link></h1>
+      <h2> <Link className='project'  to={'/projects/'}> Projects</Link></h2> 
+      <h2> <Link className='about'    to={'/about'}>     About</Link></h2>
+      <h2> <Link className='resume'   to={'/resume'}>    Resume</Link></h2>
+    </nav>  
     <main>
-  <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
-  <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
-  <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
+      <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
+      <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
+      <Route path='/project' exact render={routerProps => <Projects {...routerProps}/> } />
     </main>
   </div>
   );
