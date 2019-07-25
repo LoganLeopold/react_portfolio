57cac4b67a565593a720526101fee77929c0c02a
Logan Leopold <loganleopld@me.com>
2019-07-25 19:45:22 -0400

    trying to get hover link to keep space to present amidst layout

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
   <Container>
     <Row>
-      <Col><Link className='homepage underline' to={'/'}>          Logan</Link></Col>
-      <Col><Link className='projects underline' to={'/projects/'}> Projects</Link> </Col>
-      <Col><Link className='about underline'    to={'/about'}>     About</Link></Col>
-      <Col><Link className='resume underline'   to={'/resume'}>    Resume</Link></Col>
+      <Col><Link className='homepage' to={'/'}>          Logan</Link></Col>
+      <Col><Link className='projects' to={'/projects/'}> Projects</Link> </Col>
+      <Col><Link className='about'    to={'/about'}>     About</Link></Col>
+      <Col><Link className='resume'   to={'/resume'}>    Resume</Link></Col>
     </Row>
     <main>
       <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
