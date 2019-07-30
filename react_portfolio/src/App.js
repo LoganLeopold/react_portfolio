e41068ca6aa8aeff106a3a43efdd0ce4cf651b74
Logan Leopold <loganleopld@me.com>
2019-07-30 12:56:33 -0400

    trying a sidebar strategy

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './App.css';
 import {Link, Route} from 'react-router-dom'
 import Projects from "./Projects"
-import { Row, Container, Col } from "reactstrap";
+import About from './About'
+import Resume from './Resume'
+// import { Row, Container, Col } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link className='homepage' to={'/'}>          Logan</Link>
-        <Link className='projects' to={'/projects/'}> Projects</Link>
-        <Link className='about'    to={'/about'}>     About</Link>
-        <Link className='resume'   to={'/resume'}>    Resume</Link>
+        <Link className='homepage' to={'/'}>Logan</Link>
+        <Link className='projects' to={'/projects/'}>Projects</Link>
+        <Link className='about'    to={'/about'}>About</Link>
+        <Link className='resume'   to={'/resume'}>Resume</Link>
       </nav>
       <main>
         <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
+        <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
+        <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
       </main>
     </div>
   );
---
+++ react_portfolio/src/Resume.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    render() {
+        return (
+            <div>
+                This will show my resume for dl \m/
+            </div>
+        );
+    }
+}
+
+export default Resume;
