4f1e258b979336a647e517c336c32733bd91c650
Logan Leopold <loganleopld@me.com>
2019-07-30 13:44:40 -0400

    bootstrap grid well done, just want to get the nav into proper proportion while being responsive

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -4,23 +4,28 @@ import {Link, Route} from 'react-router-dom'
 import Projects from "./Projects"
 import About from './About'
 import Resume from './Resume'
-// import { Row, Container, Col } from "reactstrap";
+import { Row, Container, Col } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <Link className='homepage' to={'/'}>Logan</Link>
-        <Link className='projects' to={'/projects/'}>Projects</Link>
-        <Link className='about'    to={'/about'}>About</Link>
-        <Link className='resume'   to={'/resume'}>Resume</Link>
-      </nav>
-      <main>
-        <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
-        <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
-        <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
-      </main>
-    </div>
+    <Container>
+      <Row>
+        <header>Tester</header>
+      </Row>
+      <Row>
+        <Col sm={12} md={2} lg={2} xl={2} className='nav'>
+          <Link className='homepage' to={'/'}>Logan</Link>
+          <Link className='projects' to={'/projects/'}>Projects</Link>
+          <Link className='about'    to={'/about'}>About</Link>
+          <Link className='resume'   to={'/resume'}>Resume</Link>
+        </Col>
+        <Col sm={12} md={10} lg={10} xl={10} className='main'>
+          <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
+          <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
+          <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
