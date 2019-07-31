ddca198f513a029986e3c35a36b41f044793f3d3
Logan Leopold <loganleopld@me.com>
2019-07-30 21:39:53 -0400

    got imgs loading in but lots of styling + architecting on the visual side left to do

--- react_portfolio/src/Project.js
+++ react_portfolio/src/Project.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Project.css'
 
 class Project extends Component {
 
     render() {
         return (
             <div>
-                <h2>{this.props.data.url}</h2>
-                <h2>{this.props.data.img}</h2>
+                <h2 class='projectTitle'>{this.props.data.url}</h2>
+                <img class='projectImg' src={this.props.data.img} alt=''></img>
             </div>
         );
     }
