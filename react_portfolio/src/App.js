5743fe15407bcc27c85eef6d809bbf5f5c46018e
Logan Leopold <loganleopld@me.com>
2019-08-08 18:25:21 -0400

    messing with background styliing

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import {Link, Route} from 'react-router-dom'
-import Projects from "./Projects"
+import Home from './Home'
+import Projects from './Projects'
 import About from './About'
 import Resume from './Resume'
 import { Row, Container, Col } from "reactstrap";
@@ -14,11 +15,12 @@ function App() {
           <Link className='homepage masterLink' to={'/'}>Logan</Link>
           <Link className='projects masterLink' to={'/projects/'}>Projects</Link>
           <Link className='about masterLink'    to={'/about'}>About</Link>
-          <Link className='resume masterLink'   to={'/resume'}>Resume</Link>
+          <Link className='resume masterLink'   to={'/resume'}>Resume</Link>        
         </Col>
       </Row> 
       <Row className='main'> 
         <Col>
+          <Route path='/' exact render={routerProps => <Home {...routerProps}/> } />
           <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
           <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
           <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
