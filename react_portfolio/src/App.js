960f641e177ba0959a640d62e776b2c22f07c758
Logan Leopold <loganleopld@me.com>
2019-08-13 16:29:18 -0400

    good working flat page with me as background

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -25,11 +25,6 @@ function App() {
           <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
           <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
         </Col>  
-        {/* <Col className='photo'>
-          <h3 className='instaHandle'>
-            Photography by <a href='http://www.instagram.com/loganleopold' className='instaLink'>Logan Leopold</a>
-          </h3>
-        </Col> */}
       </Row>
     </Container>
   );
