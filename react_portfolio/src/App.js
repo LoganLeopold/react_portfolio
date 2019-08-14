c1cac0cd9e79ff3cf90afb8f6447f34b04355db6
Logan Leopold <loganleopld@me.com>
2019-08-13 23:32:58 -0400

    working on some special functionality for the nav

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 import {Link, Route} from 'react-router-dom'
 import Home from './Home'
@@ -6,31 +6,77 @@ import Projects from './Projects'
 import About from './About'
 import Resume from './Resume'
 import { Row, Container, Col } from "reactstrap";
+import './images/coffee.jpg'
 
-function App() {
-  return (
-    <Container className='App'>
-      <Row className='nav'>
-        <Col className='nav'>
-          <Link className='homepage masterLink' to={'/'}>Logan</Link>
-          <Link className='projects masterLink' to={'/projects/'}>Projects</Link>
-          <Link className='about masterLink'    to={'/about'}>About</Link>
-          <Link className='resume masterLink'   to={'/resume'}>Resume</Link>        
-        </Col>
-        <Col className='main'>
-          <Route path='/' exact render={routerProps => <Home {...routerProps}/> } />
-          <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
-          <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
-          <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
-        </Col>  
-        <Col className='photo'>
-          <h3 className='instaHandle'>
-            Photography by <a href='http://www.instagram.com/loganleopold' className='instaLink'>Logan Leopold</a>
-          </h3>
-        </Col>
-      </Row>
-    </Container>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Home: '',
+      Projects: '',
+      About: '',
+      Resume: '',
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.changeLinkBackground = this.changeLinkBackground.bind(this);
+    this.changeLinkText = this.changeLinkText.bind(this)
+  }
+
+  componentDidMount () {
+    this.setState({
+      Home: 'Logan',
+      Projects: 'Projects',
+      About: 'About',
+      Resume: 'Resume',
+    })
+  }
+
+  changeLinkBackground (event) { 
+
+    let projectsImg = <img src='./images/coffee.jpg' alt='' className='imgLink'></img>
+  
+    let state = event.target.name
+    
+    this.setState({
+      [state]: projectsImg
+    })
+  }
+
+  changeLinkText (event) {
+    let state = event.target.name
+
+    this.setState({
+      [state]: state
+    })
+  }
+
+  render() {
+    return (
+      <Container className='App'>
+        <Row className='nav'>
+          <Col className='nav'>
+            <Link className='homepage masterLink' name='Home' to={'/'}>{this.state.Home}</Link>
+            <Link className='projects masterLink' name='Projects' onMouseEnter={this.changeLinkBackground.bind(this)} 
+            // onMouseLeave={this.changeLinkText.bind(this)} 
+            to={'/projects/'}>{this.state.Projects}</Link>
+            <Link className='about masterLink'    name='About' to={'/about'}>{this.state.About}</Link>
+            <Link className='resume masterLink'   name='Resume' to={'/resume'}>{this.state.Resume}</Link>        
+          </Col>
+          <Col className='main'>
+            <Route path='/' exact render={routerProps => <Home {...routerProps}/> } />
+            <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
+            <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
+            <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
+          </Col>  
+          <Col className='photo'>
+            <h3 className='instaHandle'>
+              Photography by <a href='http://www.instagram.com/loganleopold' className='instaLink'>Logan Leopold</a>
+            </h3>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 
