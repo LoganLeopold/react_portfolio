5427328de91156704ed98b3ae712fd55e513b87a
Logan Leopold <loganleopld@me.com>
2019-08-26 18:35:13 -0400

    commiting to try revert again clean

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,152 +1,101 @@
 import React, { Component } from "react";
-import "./App.css";
-import { Link, Route } from "react-router-dom";
-import Home from "./Home";
-import Projects from "./Projects";
-import About from "./About";
-import Resume from "./Resume";
+import './App.css';
+import {Link, Route} from 'react-router-dom'
+import Home from './Home'
+import Projects from './Projects'
+import About from './About'
+import Resume from './Resume'
 import { Row, Container, Col } from "reactstrap";
 import Logan from './images/logan.png'
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      Home: ["masterLink", "Home", './images/logan.png'],
-      Projects: ["masterLink", "Projects", './images/logan.png'],
-      About: ["masterLink", "About", './images/logan.png'],
-      Resume: ["masterLink", "Resume", './images/logan.png']
+      Home: ['masterLink', 'Home'],
+      Projects: ['masterLink', 'Projects'],
+      About: ['masterLink', 'About'],
+      Resume: ['masterLink', 'Resume'],
     };
     this.componentDidMount = this.componentDidMount.bind(this);
     this.mouseHovers = this.mouseHovers.bind(this);
-    this.mouseExits = this.mouseExits.bind(this);
+    this.mouseExits = this.mouseExits.bind(this)
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.setState({
-      Home: ["masterLink", "Home", './images/logan.png'],
-      Projects: ["masterLink", "Projects", './images/logan.png'],
-      About: ["masterLink", "About", './images/logan.png'],
-      Resume: ["masterLink", "Resume", './images/logan.png'],
+      Home: ['masterLink', 'Home'],
+      Projects: ['masterLink', 'Projects'],
+      About: ['masterLink', 'About'],
+      Resume: ['masterLink', 'Resume'],
     });
   }
 
-  mouseHovers(event) {
-    event.preventDefault();
-
-    var url = this.state[event.target.name][2]
-    console.log(url)
-
-    var navHover = {
-      background: 'url(' + url + ')',
-    } 
-
-    this.setState(
-      {
-      [event.target.name]: ["navHover", event.target.name, url, navHover]
-    });
-
-  }
-  
-  mouseExits(event) {
-    event.preventDefault();
-
-    var url = this.state[event.target.name][2]
-
+  mouseHovers (event) { 
+    event.preventDefault()
+    console.log('ENTER')
     this.setState({
-      [event.target.name]: ["masterLink", event.target.name, url]
-    });
+      [event.target.name]: ['navHover',event.target.name]
+    })
+  }
 
-    console.log(this.state[event.target.name])
-  
+  mouseExits (event) {
+    event.preventDefault()
+    console.log('EXIT')
+    this.setState({
+      [event.target.name]: ['masterLink',event.target.name]
+    })
   }
 
   render() {
+
     return (
-      <Container className="App">
+      <Container className='App'>
         <Row>
-          <Col className="nav">
-            <div className="linkWrapper">
-              <Link
-                className={this.state.Home[0]}
-                style={this.state.Home[3]}
-                name="Home"
-                to={"/"}
-                onMouseEnter={this.mouseHovers}
-                onMouseLeave={this.mouseExits}
-              >
-                {this.state.Home[1]}
-              </Link>
-            </div>
-            <div className="linkWrapper">
-              <Link
-                className={this.state.Projects[0]}
-                style={this.state.Projects[3]}
-                name="Projects"
-                to={"/projects/"}
-                onMouseEnter={this.mouseHovers}
-                onMouseLeave={this.mouseExits}
-              >
-                {this.state.Projects[1]}
-              </Link>
-            </div>
-            <div className="linkWrapper">
-              <Link
-                className={this.state.About[0]}
-                style={this.state.About[3]}
-                name="About"
-                to={"/about"}
-                onMouseEnter={this.mouseHovers}
-                onMouseLeave={this.mouseExits}
-              >
-                {this.state.About[1]}
-              </Link>
-            </div>
-            <div className="linkWrapper">
-              <Link
-                className={this.state.Resume[0]}
-                // style={this.state.Resume[3]}
-                style={{backgroundImage: `url(${Logan})`}}
-                name="Resume"
-                to={"/resume"}
-                onMouseEnter={this.mouseHovers}
-                onMouseLeave={this.mouseExits}
-              >
-                {this.state.Resume[1]}
-              </Link>
-            </div>
+          <Col className='nav'>
+            <div className='linkWrapper'><Link
+            className={this.state.Home[0]}
+            name='Home'
+            to={'/'}
+            onMouseEnter={this.mouseHovers}
+            onMouseLeave={this.mouseExits}>
+            {this.state.Home[1]}
+            </Link></div>
+            <div className='linkWrapper'><Link
+            className={this.state.Projects[0]}
+            name='Projects'
+            to={'/projects/'}
+            onMouseEnter={this.mouseHovers}
+            onMouseLeave={this.mouseExits}>
+            {this.state.Projects[1]}
+            </Link></div>
+            <div className='linkWrapper'><Link
+            className={this.state.About[0]}
+            name='About'
+            to={'/about'}
+            onMouseEnter={this.mouseHovers}
+            onMouseLeave={this.mouseExits}>
+            {this.state.About[1]}
+            </Link></div>
+            <div className='linkWrapper'><Link
+            className={this.state.Resume[0]}
+            name='Resume'
+            to={'/resume'}
+            onMouseEnter={this.mouseHovers}
+            onMouseLeave={this.mouseExits}>
+            {this.state.Resume[1]}
+            </Link></div>        
           </Col>
-          <Col className="main">
-            <Route
-              path="/"
-              exact
-              render={routerProps => <Home {...routerProps} />}
-            />
-            <Route
-              path="/projects"
-              exact
-              render={routerProps => <Projects {...routerProps} />}
-            />
-            <Route
-              path="/about"
-              exact
-              render={routerProps => <About {...routerProps} />}
-            />
-            <Route
-              path="/resume"
-              exact
-              render={routerProps => <Resume {...routerProps} />}
-            />
-          </Col>
-          <Col className="photo">
-            <h3 className="instaHandle">
-              Photography by{" "}
-              <a
-                href="http://www.instagram.com/loganleopold"
-                className="instaLink"
-              >
-                Logan Leopold
-              </a>
+          <Col className='main'>
+            <Route path='/' exact render={routerProps => <Home {...routerProps}/> } />
+            <Route path='/projects' exact render={routerProps => <Projects {...routerProps}/> } />
+            <Route path='/about' exact render={routerProps => <About {...routerProps}/> } />
+            <Route path='/resume' exact render={routerProps => <Resume {...routerProps}/> } />
+          </Col>  
+          <Col className='photo'>
+            <h3 className='instaHandle'>
+              Photography by <a href='http://www.instagram.com/loganleopold' className='instaLink'>Logan Leopold</a>
             </h3>
           </Col>
         </Row>
@@ -155,46 +104,5 @@ class App extends Component {
   }
 }
 
+
 export default App;
-
-
-     // margin: '0 3vw',
-      // padding: '0',
-    
-      // color: 'rgba(0, 0, 0, 0)',
-      // fontSize: '3vh',
-      // fontWeight: 'bold',
-      // textShadow: '0px 1.5px 2px rgb(0, 0, 0, 0)',
-    
-      // display: 'flex',
-      // alignItems: 'center',
-      // textAlign: 'center',
-      // justifyContent: 'center',
-    
-      // height: '15vh',
-      // width: '6.5vw',
-    
-      // textDecoration: 'none',
-    
-      // backgroundPosition: 'top center',
-      // backgroundSize: '90%',   
-
-
-  // var masterLink = {
-  //   margin: '0 3vw',
-  //   padding: '0',
-  
-  //   color: 'white',
-  //   fontSize: '3vh',
-  //   fontWeight: 'bold',
-  //   textShadow: '0px 1.5px 2px rgb(0, 0, 0, .08)',
-  
-  //   display: 'flex',
-  //   textAlign: 'center',
-  //   justifyContent: 'center',
-  //   alignContent: 'center',
-  //   alignItems: 'center',
-  
-  //   height: '15vh',
-  //   width: '6.5vw',
-  // }
