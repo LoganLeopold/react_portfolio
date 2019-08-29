ce18462e015d2613c0d630b4ded24b59b0082a82
Logan Leopold <loganleopld@me.com>
2019-08-29 17:59:39 -0400

    project view styling

--- react_portfolio/src/Project.js
+++ react_portfolio/src/Project.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Row, Container, Col } from "reactstrap";
-import './Project.css'
+import "./Project.css";
 
 class Project extends Component {
-
-    render() {
-        return (
-            <Container className='projectCard'>
-                <Row className='projectImgCenter'>
-                  <Col className='projectImgCenter'>
-                      <a href={this.props.data.url} className='projectLink'>
-                          <h2 class='projectTitle'>{this.props.data.title}</h2>
-                      </a>
-                  </Col>
-                </Row>
-                <Row className='projectCenter'>
-                  <Col className='projectCenter'>
-                      <a href={this.props.data.url} className='projectLink'>
-                          <img class='projectImg' src={this.props.data.img} alt=''></img>
-                      </a>
-                  </Col>
-                </Row>
-            </Container>
-        );
-    }
+  render() {
+    return (
+        <Container className="projectCard">
+          <Row className="projectImgCenter">
+            <Col className="projectImgCenter">
+              <a href={this.props.data.url} className="projectLink">
+                <h2 class="projectTitle">{this.props.data.title}</h2>
+              </a>
+            </Col>
+          </Row>
+          <Row className="projectCenter">
+            <Col className="projectCenter">
+              <a href={this.props.data.url} className="projectLink">
+                <img class="projectImg" src={this.props.data.img} alt="" />
+              </a>
+            </Col>
+          </Row>
+        </Container>
+    );
+  }
 }
 
 export default Project;
