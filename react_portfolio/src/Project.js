804c9c03e332ab45e77b1d02972e993deb04bb35
Logan Leopold <loganleopld@me.com>
2019-08-30 12:29:00 -0400

    project grid seems to be working great responsively

--- react_portfolio/src/Project.js
+++ react_portfolio/src/Project.js
@@ -6,15 +6,15 @@ class Project extends Component {
   render() {
     return (
         <Container className="projectCard">
-          <Row className="projectImgCenter">
-            <Col className="projectImgCenter">
+          <Row className="projectCenter">
+            <Col className="projectCenter" sm={12} md={12} lg={12} xl={12}>
               <a href={this.props.data.url} className="projectLink">
                 <h2 class="projectTitle">{this.props.data.title}</h2>
               </a>
             </Col>
           </Row>
           <Row className="projectCenter">
-            <Col className="projectCenter">
+            <Col className="projectCenter" sm={12} md={12} lg={12} xl={12}>
               <a href={this.props.data.url} className="projectLink">
                 <img class="projectImg" src={this.props.data.img} alt="" />
               </a>
